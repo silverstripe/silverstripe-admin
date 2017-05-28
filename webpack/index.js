8df646a4af6cdc35a50a2fded35c3a74f5ada644
Aaron Carlino <unclecheese@leftandmain.com>
2017-05-29 11:22:49 +1200

    remove webpack folder, ugrade to SS webpack package

--- webpack/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  moduleCSS: require('./css/modules'),
-  pluginCSS: require('./css/plugins'),
-
-  externalJS: require('./js/externals'),
-  moduleJS: require('./js/modules'),
-  pluginJS: require('./js/plugins'),
-  resolveJS: require('./js/resolve'),
-};
