1b192c3f1c5be950716eaaeef4210ab221dfa886
Christopher Joe <flamer.ohr@gmail.com>
2018-01-24 12:48:36 +1300

    Add provideContext to exported modules in injector

--- client/src/lib/Injector.js
+++ client/src/lib/Injector.js
@@ -1,11 +1,13 @@
 import provideInjector from './dependency-injection/provideInjector';
+import provideContext from './dependency-injection/provideContext';
 import withInjector from './dependency-injection/withInjector';
 import inject from './dependency-injection/inject';
-import Container from './dependency-injection/Container';
 import loadComponent from './dependency-injection/loadComponent';
+import Container from './dependency-injection/Container';
 
 export {
   provideInjector,
+  provideContext,
   withInjector,
   inject,
   loadComponent,
