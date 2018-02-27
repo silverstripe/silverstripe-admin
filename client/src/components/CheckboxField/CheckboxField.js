e7698266864dfeacdd7b7b87900bddc44ea21447
Damian Mooyman <damian@silverstripe.com>
2018-02-27 16:53:21 +1300

    Restore checkbox fieldholder

--- client/src/components/CheckboxField/CheckboxField.js
+++ client/src/components/CheckboxField/CheckboxField.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import fieldHolder from 'components/FieldHolder/FieldHolder';
 import OptionField from 'components/OptionsetField/OptionField';
 
+// Build inner field
 const CheckboxField = (props) => (
   <OptionField {...props} type="checkbox" />
 );
 
-export default CheckboxField;
+// Build holder
+const CheckboxWithHolder = (props) => {
+  const FieldHolder = fieldHolder(CheckboxField);
+  // set to not show field holder labels, as checkbox already generates a label
+  return <FieldHolder {...props} hideLabels />;
+};
+
+export { CheckboxField as Component };
+
+export default CheckboxWithHolder;
