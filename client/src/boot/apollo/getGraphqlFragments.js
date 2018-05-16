5b2e4b19923c1592139442ed5f161e5d00f2a8d3
Aaron Carlino <unclecheese@leftandmain.com>
2018-05-16 16:38:21 +1200

    Add graceful fallback when static graphql schema not present

--- client/src/boot/apollo/getGraphqlFragments.js
+++ client/src/boot/apollo/getGraphqlFragments.js
@@ -1,22 +1,56 @@
 import fetch from 'isomorphic-fetch';
 
-const getGraphqlFragments = async (baseUrl) => (
-  fetch(`${baseUrl}/assets/types.graphql`, {
+const parseResponse = result => {
+  const fragmentData = result.data;
+  fragmentData.__schema.types = fragmentData.__schema.types.filter(
+    type => type.possibleTypes !== null,
+  );
+  return fragmentData;
+};
+
+const handleError = response => {
+  if (!response.ok) {
+    throw new Error(
+      `The types.graphql file could not be loaded. You probably need to run a ?flush to generate it.
+            Alternatively, you can use the IntrospectionProvider extension to generate the file dynamically.
+            More information: https://github.com/silverstripe/silverstripe-graphql/#schema-introspection`
+    );
+  }
+
+  return response;
+};
+
+const getGraphqlFragments = async (baseUrl) => {
+  const staticURL = `${baseUrl}assets/types.graphql`;
+  const dynamicURL = `${baseUrl}graphql/types`;
+  const fetchConfig = {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json'
     },
     uri: `${baseUrl}`,
     credentials: 'same-origin',
-  })
-    .then(result => result.json())
-    .then(result => {
-      const fragmentData = result.data;
-      fragmentData.__schema.types = fragmentData.__schema.types.filter(
-        type => type.possibleTypes !== null,
-      );
-      return fragmentData;
-    })
-);
+  };
+
+  const fetchSchema = async (url) => (
+    fetch(url, fetchConfig)
+      .then(handleError)
+      .then(result => result.json())
+      .then(parseResponse)
+  );
+
+  let response;
+  try {
+    response = await fetchSchema(staticURL);
+  } catch (e) {
+    try {
+      response = await fetchSchema(dynamicURL);
+    } catch (finalError) {
+      return Promise.reject(finalError);
+    }
+  }
+
+  return Promise.resolve(response);
+};
 
 export default getGraphqlFragments;
