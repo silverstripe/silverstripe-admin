f9f904a3f513b6fc65acad96fe17f03f0b36e37c
Luke Edwards <luke@silverstripe.com>
2018-07-11 11:10:36 +1200

    More removal, update bundle

--- client/src/legacy/GridField.js
+++ client/src/legacy/GridField.js
@@ -314,23 +314,9 @@ $.entwine('ss', function($) {
         filterState='hidden';
       }
 
-      if (this.hasClass('action-restore')) {
-        var successCallback = function(data, status, response) {
-          const messageText = response.getResponseHeader('X-Message-Text');
-          const messageType = response.getResponseHeader('X-Message-Type');
-          if (messageText && messageType) {
-            $("#Form_EditForm_error").addClass(messageType);
-            $("#Form_EditForm_error").html(messageText);
-            $("#Form_EditForm_error").show();
-          }
-        }
-      }
-
-      this.getGridField().reload(
-        {
-          data: [{ name: this.attr('name'), value: this.val(), filter: filterState }],
-        },
-        successCallback);
+      this.getGridField().reload({
+        data: [{name: this.attr('name'), value: this.val(), filter: filterState}]
+      });
 
       e.preventDefault();
     },
