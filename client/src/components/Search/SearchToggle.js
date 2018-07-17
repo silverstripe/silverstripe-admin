43f7db6ecc527f43e2cdb259bacbda312e1fd845
Maxime Rainville <maxime@silverstripe.com>
2018-07-17 18:15:58 +1200

    Tweak the search Toogle so it can be used outside of Search.

--- client/src/components/Search/SearchToggle.js
+++ client/src/components/Search/SearchToggle.js
@@ -1,13 +1,22 @@
 import i18n from 'i18n';
 import React, { PropTypes } from 'react';
+import classnames from 'classnames';
 
 const label = i18n._t('Admin.SHOW_SEARCH', 'Show Search');
+const btnClasses = classnames(
+  'btn',
+  'btn--no-text',
+  'btn-secondary',
+  'search__trigger',
+  'font-icon-search',
+  'btn--icon-large'
+);
 
 
-const SearchToggle = ({ onToggle }) => (<button
+const SearchToggle = ({ onToggle, toggled }) => (<button
   title={label}
   onClick={onToggle}
-  className="btn btn--no-text btn-secondary search__trigger font-icon-search btn--icon-large"
+  className={btnClasses + (toggled ? ' search__trigger-active' : '')}
 >
   <span className="sr-only">{label}</span>
 </button>);
@@ -15,6 +24,7 @@ const SearchToggle = ({ onToggle }) => (<button
 
 SearchToggle.propTypes = {
   onToggle: PropTypes.func,
+  toggled: PropTypes.bool
 };
 
 export { SearchToggle as Component };
