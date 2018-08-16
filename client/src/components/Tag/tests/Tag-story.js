4a3c6504d647b7512dace81147942f0b0bc54dfa
Maxime Rainville <maxime@silverstripe.com>
2018-08-17 00:01:36 +1200

    Finalise the Tag pattern lib entry.

--- client/src/components/Tag/tests/Tag-story.js
+++ client/src/components/Tag/tests/Tag-story.js
@@ -1,44 +1,61 @@
 import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { storiesOf } from '@storybook/react';
-import Tag from '../Tag';
-import TagList from '../TagList';
-import CompactTagList from '../CompactTagList';
+import { storiesOf, setAddon } from '@storybook/react';
+import actionListMaker from '../../../stories/actionListMaker';
+import { withKnobs, text, boolean, number, object } from '@storybook/addon-knobs/react';
+import Tag from 'components/Tag//Tag';
+import TagList from 'components/Tag//TagList';
+import CompactTagList from 'components/Tag//CompactTagList';
+import JSXAddon from 'storybook-addon-jsx';
+
+setAddon(JSXAddon);
 
 const tags = [
   {key: 'justKey'},
   {key: 'KeyPair', value: '123'},
-  {key: 'KeyLabel', label: "Some label"},
-  {key: 'KeyPairLabel', label: "Some label", value: '123'}
+  {key: 'KeyLabel', label: 'Some label'},
+  {key: 'KeyPairLabel', label: 'Some label', value: '123'}
 ];
 
-const moreTags = [
-  {key: 'MoreValue', value: '123'},
-  {key: 'ExtraValue', value: '123'},
-  {key: 'MoreExtraValue', value: '123'},
-  {key: 'ReallyALotOfValue', value: '123'},
-].concat(tags);
+const tagActions = actionListMaker('onClick', 'onDelete', 'onDeleteKey', 'onBackSpace', 'onNext', 'onPrevious');
+const tagListActions = actionListMaker('onHolderFocus', 'onTagClick', 'onTagDelete');
+const compactTagListActions = actionListMaker('onSummary');
+
+
 
 storiesOf('Admin/Tag', module)
-  .add('Tag', () => (
+  .addDecorator(story => (
     <div style={{"background": 'white', 'padding': '20px'}}>
-      <Tag dataKey={"HelloWorld"} />
-      <Tag label="Hello World" />
-      <Tag dataKey="Deletable" deletable />
-      <Tag dataKey="KeyPair" value="SomeValue123" />
-      <Tag dataKey="DeletableKeyPair" value="SomeValue123" deletable />
+      {story()}
     </div>
   ))
-  .add('TagList', () => (
-    <div style={{"background": 'white', 'padding': '20px'}}>
-      <TagList tags={tags} />
-      <TagList tags={tags} deletable />
-    </div>
+  .addDecorator(withKnobs)
+  .addWithJSX('Tag', () => (
+      <Tag
+        dataKey={text('dataKey', 'HelloWorld')}
+        label={text('label', '')}
+        value={text('value', '')}
+        deletable={boolean('Deletable', false)}
+        focusable={boolean('focusable', true)}
+        {...tagActions}
+       />
   ))
-  .add('CompactTagList', () => (
-    <div style={{"background": 'white', 'padding': '20px'}}>
-      <CompactTagList tags={tags} deletable maxSize={600} />
-      <CompactTagList tags={moreTags} deletable maxSize={600} />
-    </div>
+  .addWithJSX('TagList', () => (
+    <TagList
+      deletable={boolean('Deletable', false)}
+      focusable={boolean('focusable', true)}
+      tags={tags}
+      {...tagListActions}
+    />
+  ))
+  .addWithJSX('CompactTagList', () => (
+    <CompactTagList
+      deletable={boolean('Deletable', false)}
+      focusable={boolean('focusable', true)}
+      maxSize={number('maxSize', 200)}
+      tags={object('tags', tags)}
+      {...tagListActions}
+      {...compactTagListActions}
+    />
   ));
 
