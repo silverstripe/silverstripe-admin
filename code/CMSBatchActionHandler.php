1d6666333174d737054dca83010302644fbbf7ec
Robbie Averill <robbie@silverstripe.com>
2018-08-20 23:08:28 +1200

    FIX Ambigious RecordID columns is now specified against owner's base table verisons

--- code/CMSBatchActionHandler.php
+++ code/CMSBatchActionHandler.php
@@ -327,9 +327,10 @@ class CMSBatchActionHandler extends RequestHandler
         if ($recordClass::has_extension(Versioned::class)) {
             // Workaround for get_including_deleted not supporting byIDs filter very well
             // Ensure we select both stage / live records
-            $pages = Versioned::get_including_deleted($recordClass, array(
-                '"RecordID" IN ('.DB::placeholders($ids).')' => $ids
-            ));
+            $tableName = DataObject::getSchema()->baseDataTable($recordClass) . '_Versions';
+            $pages = Versioned::get_including_deleted($recordClass, [
+                '"' . $tableName . '"."RecordID" IN (' . DB::placeholders($ids) . ')' => $ids
+            ]);
         } else {
             $pages = DataObject::get($recordClass)->byIDs($ids);
         }
