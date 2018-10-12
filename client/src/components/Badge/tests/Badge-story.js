2a30676f85ab0713f2aede811c097fac26f4a57e
Maxime Rainville <maxime@silverstripe.com>
2018-10-12 17:36:00 +1300

    Minor Improve badge story in pattern lib

--- client/src/components/Badge/tests/Badge-story.js
+++ client/src/components/Badge/tests/Badge-story.js
@@ -1,83 +1,22 @@
 import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { storiesOf } from '@storybook/react';
-import Badge from 'components/Badge/Badge';
+import { storiesOf, setAddon } from '@storybook/react';
+import { withKnobs, text, boolean, select } from '@storybook/addon-knobs/react';
+import JSXAddon from 'storybook-addon-jsx';
 
-storiesOf('Admin/Badge', module)
-  .add('Default states', () => (
-    <div>
-      <div>
-        <Badge message="Default badge" />
-      </div>
-      <div>
-        <Badge status="info" message="Info badge" />
-      </div>
-      <div>
-        <Badge status="success" message="Success badge" />
-      </div>
-      <div>
-        <Badge status="warning" message="Warning badge" />
-      </div>
-      <div>
-        <Badge status="danger" message="Danger badge" />
-      </div>
-      <div>
-        <Badge status="primary" message="Primary badge" />
-      </div>
-      <div>
-        <Badge status="secondary" message="Secondary badge" />
-      </div>
-    </div>
-  ))
-  .add('Non-pill states', () => (
-    <div>
-      <div>
-        <div>
-          <Badge message="Default badge" className="" />
-        </div>
-        <div>
-          <Badge status="info" message="Info badge" className="" />
-        </div>
-        <div>
-          <Badge status="success" message="Success badge" className="" />
-        </div>
-        <div>
-          <Badge status="warning" message="Warning badge" className="" />
-        </div>
-        <div>
-          <Badge status="danger" message="Danger badge" className="" />
-        </div>
-        <div>
-          <Badge status="primary" message="Primary badge" className="" />
-        </div>
-        <div>
-          <Badge status="secondary" message="Secondary badge" className="" />
-        </div>
-      </div>
-    </div>
-  ))
-  .add('Inverted states', () => (
-    <div>
-      <div>
-        <Badge message="Default (inverted)" inverted />
-      </div>
-      <div>
-        <Badge status="info" message="Info badge (inverted)" inverted />
-      </div>
-      <div>
-        <Badge status="success" message="Success badge (inverted)" inverted />
-      </div>
-      <div>
-        <Badge status="warning" message="Warning badge (inverted)" inverted />
-      </div>
-      <div>
-        <Badge status="danger" message="Danger badge (inverted)" inverted />
-      </div>
-      <div>
-        <Badge status="primary" message="Primary badge (inverted)" inverted />
-      </div>
-      <div>
-        <Badge status="secondary" message="Secondary badge (inverted)" inverted />
-      </div>
-    </div>
+import Badge, { statuses } from 'components/Badge/Badge';
+
+
+setAddon(JSXAddon);
+
+storiesOf('Admin', module)
+  .addDecorator(story => <div>{story()}</div>)
+  .addDecorator(withKnobs)
+  .addWithJSX('Badge', () => (
+    <Badge
+      message={text('message', 'Hello World')}
+      status={select('status', statuses, 'default')}
+      className={boolean('pill', true) ? 'badge-pill' : ''}
+      inverted={boolean('inverted')}
+    />
   ));
