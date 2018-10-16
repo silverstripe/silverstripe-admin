563d25b2aef93a19829c415f607a02870842b012
Maxime Rainville <maxime@silverstripe.com>
2018-10-17 12:27:00 +1300

    MINOR Convert CheckboxFieldSet story to JSX.

--- client/src/components/CheckboxSetField/tests/CheckboxSetField-story.js
+++ client/src/components/CheckboxSetField/tests/CheckboxSetField-story.js
@@ -1,28 +1,46 @@
 import React from 'react';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { storiesOf } from '@storybook/react';
-import CheckboxSetField from 'components/CheckboxSetField/CheckboxSetField';
+import { storiesOf, setAddon } from '@storybook/react';
+import {withKnobs, text, boolean, number, object, select} from '@storybook/addon-knobs/react';
+import JSXAddon from 'storybook-addon-jsx';
 import ValueTracker from 'stories/ValueTracker';
+import CheckboxSetField from 'components/CheckboxSetField/CheckboxSetField';
+
+setAddon(JSXAddon);
 
 const props = {
   id: 'set',
   title: '',
   name: 'set',
   value: ['3'],
-  source: [
-    { value: '1', title: 'one' },
-    { value: '2', title: 'two' },
-    { value: '3', title: 'three' },
-    { value: '4', title: 'four' },
-  ],
 };
+
+const source = [
+  { value: '1', title: 'one' },
+  { value: '2', title: 'two' },
+  { value: '3', title: 'three' },
+  { value: '4', title: 'four' },
+  ];
 
 storiesOf('Admin/CheckboxSetField', module)
   .addDecorator((storyFn) => (
     <ValueTracker>{storyFn()}</ValueTracker>
   ))
-  .add('CheckboxSet', () => (
+  .addDecorator(withKnobs)
+  .addWithJSX('CheckboxSet', () => (
     <CheckboxSetField
-      {...props}
+      id={text('id', 'checkset1')}
+      name={text('name', 'checkset1')}
+      value={['3']}
+      source={object('source', source)}
+      hideLabels={boolean('hideLabels', false)}
+      title={text('title', 'Field Title')}
+      rightTitle={text('rightTitle', 'Right Title')}
+      name={text('name', 'FieldName')}
+      id={text('id', 'FieldID')}
+      description={text('description', 'Field Description')}
+      message={{
+        type: select('message type', ['', 'error'], 'error'),
+          value: text('message', 'A message about the field.') }
+      }
     />
   ));
