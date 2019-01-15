58ce2808d592dc84292a1af6675677504e44ffa6
bergice <andre.kiste@silverstripe.com>
2019-01-16 12:42:48 +1300

    Fix moment locale

--- code/LeftAndMain.php
+++ code/LeftAndMain.php
@@ -687,7 +687,7 @@ class LeftAndMain extends Controller implements PermissionProvider
         Requirements::javascript('silverstripe/admin: thirdparty/bootstrap/js/dist/collapse.js');
         Requirements::css('silverstripe/admin: client/dist/styles/bundle.css');
         Requirements::add_i18n_javascript('silverstripe/admin:client/lang');
-        Requirements::add_i18n_javascript('silverstripe/admin:client/dist/moment-locales');
+        Requirements::add_i18n_javascript('silverstripe/admin:client/dist/moment-locales', false, false, true);
 
         if (LeftAndMain::config()->uninherited('session_keepalive_ping')) {
             Requirements::javascript('silverstripe/admin: client/dist/js/LeftAndMain.Ping.js');
