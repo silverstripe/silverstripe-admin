06beff71a45bca0f42c88ea931f142d8bc10d008
Aaron Carlino <unclecheese@leftandmain.com>
2019-06-17 11:13:42 +1200

    NEW: Allow export of injected GraphQL AST alongside HOC (#889)
    * NEW: Add ApolloGraphqlProxy class to allow export of injected GraphQL AST
    
    * Add tests for ApolloGraphqlProxy
    
    * Linting

---
+++ client/src/lib/dependency-injection/tests/ApolloGraphqlProxy-test.js
@@ -0,0 +1,32 @@
+/* global jest, jasmine, describe, beforeEach, it, pit, expect, process */
+
+import ApolloGraphqlProxy from '../ApolloGraphqlProxy';
+import gql from 'graphql-tag';
+
+const createMock = (ast, config = {}) => (
+  new ApolloGraphqlProxy(ast, config)
+);
+
+describe('ApolloGraphqlProxy', () => {
+  it('Constructs', () => {
+    const proxy = createMock(
+      'ast',
+      { foo: 'bar' }
+    );
+
+    expect(proxy.getGraphqlAST()).toBe('ast');
+    expect(proxy.getApolloConfig().foo).toBe('bar');
+  });
+
+  it('Builds a HOC', () => {
+    const proxy = createMock(
+      gql('query myQuery { foo }'),
+      {
+        test: 'config'
+      }
+    );
+
+    const hoc = proxy.getApolloHOC();
+    expect(typeof hoc).toBe('function');
+  });
+});
