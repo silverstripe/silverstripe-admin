865f0fc818922f231596a7cadd2061e4624994a9
Maxime Rainville <maxime@silverstripe.com>
2019-08-15 19:10:15 +1200

    FIX Turn the Breadcrumb into pure component

--- client/src/components/Breadcrumb/Breadcrumb.js
+++ client/src/components/Breadcrumb/Breadcrumb.js
@@ -2,72 +2,57 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-class Breadcrumb extends Component {
-  getLastCrumb() {
-    return this.props.crumbs && this.props.crumbs[this.props.crumbs.length - 1];
-  }
+/**
+ * Render a single crumb
+ */
+const Crumb = ({text, href, onClick}) => (
+  <li className="breadcrumb__item">
+    <a className="breadcrumb__item-title" href={href} onClick={onClick}>
+      {text}
+    </a>
+  </li>
+);
 
-  renderBreadcrumbs() {
-    if (!this.props.crumbs) {
-      return null;
-    }
+/**
+ * Render all the crumbs except the last one
+ */
+const Crumbs = ({crumbs}) => (
+  crumbs.length == 0 ?
+    null :
+    <div className="breadcrumb__list-container">
+      <ol className="breadcrumb">
+      {crumbs.map((crumb, idx) => <Crumb key={crumb.text + idx} {...crumb} />)}
+      </ol>
+    </div>
+);
 
-    return this.props.crumbs.slice(0, -1).map((crumb) => (
-      <li key={crumb.text} className="breadcrumb__item">
-        <a
-          className="breadcrumb__item-title"
-          href={crumb.href}
-          onClick={crumb.onClick}
-        >
-          {crumb.text}
-        </a>
-      </li>
-    ));
-  }
+/**
+ * Render the primary crumb
+ */
+const CurrentCrumb = ({text, onClick, icon, label}) => (
+  <div className="breadcrumb__item breadcrumb__item--last">
+    <h2 className="breadcrumb__item-title">
+    {text}
+    {icon && (
+      <span
+        className={'breadcrumb__icon ' + (icon.className || '')}
+        role="button"
+        tabIndex={0}
+        aria-label={label}
+        onClick={icon.onClick}
+      /> )}
+    </h2>
+  </div>
+);
 
-  renderLastCrumb() {
-    const crumb = this.getLastCrumb();
-    if (!crumb) {
-      return null;
-    }
-
-    const iconClassNames = ['breadcrumb__icon'];
-    if (crumb.icon) {
-      iconClassNames.push(crumb.icon.className);
-    }
-
-    return (
-      <div className="breadcrumb__item breadcrumb__item--last">
-        <h2 className="breadcrumb__item-title">
-          {crumb.text}
-          {crumb.icon && (
-            <span
-              className={iconClassNames.join(' ')}
-              role="button"
-              tabIndex={0}
-              onClick={crumb.icon.onClick}
-            />
-          )}
-        </h2>
-      </div>
-    );
-  }
-
-  render() {
-    return (
-      <div className="breadcrumb__container fill-height flexbox-area-grow">
-        { this.props.crumbs && this.props.crumbs.length > 1 &&
-          <div className="breadcrumb__list-container">
-            <ol className="breadcrumb">
-              {this.renderBreadcrumbs()}
-            </ol>
-          </div>
-        }
-        {this.renderLastCrumb()}
-      </div>
-    );
-  }
-}
+const Breadcrumb = ({crumbs}) => (
+  <div className="breadcrumb__container fill-height flexbox-area-grow">
+    { crumbs && crumbs.length > 0 && <React.Fragment>
+      <Crumbs crumbs={[...crumbs].slice(0,-1)} />
+      <CurrentCrumb {...([...crumbs].pop())} />
+    </React.Fragment> }
+  </div>
+);
 
 Breadcrumb.propTypes = {
   crumbs: PropTypes.arrayOf(PropTypes.shape({
@@ -75,6 +60,7 @@ Breadcrumb.propTypes = {
     text: PropTypes.string,
     icon: PropTypes.shape({
       className: PropTypes.string,
+      label: PropTypes.string,
       onClick: PropTypes.func,
       action: (props) => { if (props.action) { throw new Error('action: no longer used'); } },
     })
