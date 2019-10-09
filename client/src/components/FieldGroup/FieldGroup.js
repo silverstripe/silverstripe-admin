57e481f8b864e9489d61ef24f5f22a94019b9194
Maxime Rainville <maxime@silverstripe.com>
2019-10-09 18:21:36 +1300

    BUG Add support for disabling the smallfield holder label on react FieldGroup

--- client/src/components/FieldGroup/FieldGroup.js
+++ client/src/components/FieldGroup/FieldGroup.js
@@ -1,12 +1,26 @@
 import CompositeField from 'components/CompositeField/CompositeField';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 class FieldGroup extends CompositeField {
   getClassName() {
-    return `field-group-component ${super.getClassName()}`;
+    return classnames(
+      'field-group-component',
+      { 'field-group-component__small-holder': this.props.smallholder },
+      super.getClassName()
+    );
   }
 }
 
 // Field group is essentially a composite field, but wrapped with a typical field holder
 // fieldHolder wrapping happens in the FormBuilder component
+
+FieldGroup.propTypes = Object.assign({}, CompositeField.propTypes, {
+  smallholder: PropTypes.bool
+});
+
+FieldGroup.defaultProps = Object.assign({}, CompositeField.defaultProps, {
+  smallholder: true
+});
 
 export default FieldGroup;
