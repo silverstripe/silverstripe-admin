c4c688fd479a445feb7fe2de544ff4c72576d837
Maxime Rainville <maxime@silverstripe.com>
2019-11-05 16:19:04 +1300

    BUG Do not include moment locales in bundle

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,7 @@ const config = [
     module: moduleJS(ENV, PATHS),
     plugins: [
       ...pluginJS(ENV, PATHS),
+      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
       // Most vendor libs are loaded directly into the 'vendor' bundle (through require()
       // calls in vendor.js). This ensures that any further require() calls in other
       // bundles aren't duplicating libs.
