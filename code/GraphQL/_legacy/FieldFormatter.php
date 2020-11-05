503eb0fbc04c28873830ecdbb3f95e537dfd0a4a
Aaron Carlino <unclecheese@leftandmain.com>
2020-11-06 10:42:40 +1300

    New custom formatter for id

---
+++ code/GraphQL/_legacy/FieldFormatter.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace SilverStripe\Admin\GraphQL;
+
+
+use SilverStripe\Core\Convert;
+
+class FieldFormatter
+{
+    /**
+     * A couple of hacks to make sure ID gets formatted correctly, since it's so common.
+     * @param string $field
+     * @return string
+     */
+    public static function format(string $field): string
+    {
+        if (strtolower($field) === 'id') {
+            return 'id';
+        }
+        if (strtolower($field) === 'ids') {
+            return 'ids';
+        }
+
+        return Convert::upperCamelToLowerCamel($field);
+    }
+}
