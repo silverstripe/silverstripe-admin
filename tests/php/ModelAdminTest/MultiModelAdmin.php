dc1c7cef00c1b6cce0dba861cc041e5c470f52a7
Maxime Rainville <maxime@silverstripe.com>
2021-02-04 15:12:36 +1300

    BUG Update getManagedModelTabs so it can still work with the old output format of getManagedModels (#1172)

---
+++ tests/php/ModelAdminTest/MultiModelAdmin.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace SilverStripe\Admin\Tests\ModelAdminTest;
+
+use SilverStripe\Admin\ModelAdmin;
+use SilverStripe\Control\Controller;
+use SilverStripe\Dev\TestOnly;
+
+class MultiModelAdmin extends ModelAdmin implements TestOnly
+{
+    private static $url_segment = 'multi';
+
+    private static $managed_models = [
+        Contact::class,
+        'Player' => [
+            'dataClass' => Player::class,
+            'title' => 'Ice Hockey Players'
+        ],
+        Player::class => [
+            'title' => 'Rugby Players'
+        ]
+    ];
+
+    public function Link($action = null)
+    {
+        if (!$action) {
+            $action = $this->sanitiseClassName($this->modelClass);
+        }
+        return Controller::join_links('ContactAdmin', $action, '/');
+    }
+
+    // The purpose of this method is to increase the visibility of ModelAdmin::getManagedModelTabs()
+    // from protected to public
+    public function getManagedModelTabs()
+    {
+        return parent::getManagedModelTabs();
+    }
+}
