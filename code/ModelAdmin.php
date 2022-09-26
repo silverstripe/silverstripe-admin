fd91da37c43846ffaafdd1d7b1578e7811739214
Guy Sartorelli <guy.sartorelli@silverstripe.com>
2022-09-27 11:22:50 +1300

    FIX Always add dataClass to models spec

--- code/ModelAdmin.php
+++ code/ModelAdmin.php
@@ -538,11 +538,16 @@ abstract class ModelAdmin extends LeftAndMain
 
         // Normalize models to have their model class in array key
         foreach ($models as $k => $v) {
+            // No custom tab url segment
             if (is_numeric($k)) {
                 $models[$v] = ['dataClass' => $v, 'title' => singleton($v)->i18n_plural_name()];
                 unset($models[$k]);
+            // Custom title but no custom tab url segment
             } elseif (is_array($v) && !isset($v['dataClass'])) {
                 $models[$k]['dataClass'] = $k;
+            // Custom tab url segment but no custom title
+            } elseif (is_a($v, DataObject::class, true)) {
+                $models[$k] = ['dataClass' => $v, 'title' => singleton($v)->i18n_plural_name()];
             }
         }
 
