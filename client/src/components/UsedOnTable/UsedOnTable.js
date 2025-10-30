cac0db63a3d13126419bfc642cc4d75d158bb742
Steve Boyd <emteknetnz@gmail.com>
2025-10-30 19:35:00 +1300

    ENH Convert UsedOnTable to functional component

--- client/src/components/UsedOnTable/UsedOnTable.js
+++ client/src/components/UsedOnTable/UsedOnTable.js
@@ -1,113 +1,105 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import i18n from 'i18n';
 import Loading from 'components/Loading/Loading';
 import provideUsedOnData from './provideUsedOnData';
 
-class UsedOnTable extends PureComponent {
-  renderHeader() {
-    return (
-      <thead>
-        <tr>
-          <th scope="col" className="used-on__col--index">{i18n._t('Admin.USED_ON_NUM', '#')}</th>
-          <th scope="col" className="used-on__col--title">{i18n._t('Admin.USED_ON', 'Used on')}</th>
-        </tr>
-      </thead>
-    );
-  }
+const renderHeader = () => (
+  <thead>
+    <tr>
+      <th scope="col" className="used-on__col--index">{i18n._t('Admin.USED_ON_NUM', '#')}</th>
+      <th scope="col" className="used-on__col--title">{i18n._t('Admin.USED_ON', 'Used on')}</th>
+    </tr>
+  </thead>
+);
 
-  renderBody() {
-    const { usedOn, loading, error } = this.props;
-
-    if (error || !usedOn || !usedOn.length) {
-      let message = null;
-      let classState = null;
-
-      if (error) {
-        message = i18n.inject(
-          i18n._t('Admin.LOADING_ERROR', 'As error occured when loading the data: {message}'),
-          { message: error },
-        );
-        classState = 'error';
-      } else if (loading) {
-        message = <Loading />;
-        classState = 'loading';
-      } else {
-        message = i18n._t('Admin.NOT_USED', 'This file is currently not in use.');
-        classState = 'empty';
-      }
-
-      const className = classnames([
-        'used-on__message',
-        `used-on__message--${classState}`,
-      ]);
-
-      return (
-        <tbody aria-live="polite">
-          <tr>
-            <td className={className} colSpan="3">{message}</td>
-          </tr>
-        </tbody>
-      );
+const renderRow = (data, index) => {
+  const { id, type } = data;
+  const rowData = data.ancestors
+    ? [data].concat(data.ancestors).reverse()
+    : [data].reverse();
+  let cellLink = '#';
+  let isFirst = true;
+  const titleLinks = rowData.map((arr, i) => {
+    let title = arr.title;
+    const link = arr.link;
+    if (title && title.length >= 25) {
+      title = `${title.substring(0, 25).trim()}...`;
     }
+    if (link) {
+      cellLink = link;
+    }
+    const key = `${index}-${id}-${i}`;
+    const cssClasses = ['used-on__title-item'];
+    if (isFirst) {
+      cssClasses.push('used-on__title-item--first');
+      isFirst = false;
+    }
+    return <li className={classnames(cssClasses)} key={key}>{title}</li>;
+  });
+  const key = `${index}-${id}`;
+  return (
+    <tr key={key} className="used-on__row">
+      <td className="used-on__col--index">
+        <a href={cellLink} className="used-on__cell-link">{index + 1}</a>
+      </td>
+      <td className="used-on__col--title">
+        <a href={cellLink} className="used-on__cell-link">
+          <ul className="used-on__title-items">{titleLinks}</ul>
+          <span className="used-on__type">{type}</span>
+        </a>
+      </td>
+    </tr>
+  );
+};
+
+const renderBody = (usedOn, loading, error) => {
+  if (error || !usedOn || !usedOn.length) {
+    let message = null;
+    let classState = null;
+
+    if (error) {
+      message = i18n.inject(
+        i18n._t('Admin.LOADING_ERROR', 'As error occured when loading the data: {message}'),
+        { message: error },
+      );
+      classState = 'error';
+    } else if (loading) {
+      message = <Loading />;
+      classState = 'loading';
+    } else {
+      message = i18n._t('Admin.NOT_USED', 'This file is currently not in use.');
+      classState = 'empty';
+    }
+
+    const className = classnames([
+      'used-on__message',
+      `used-on__message--${classState}`,
+    ]);
 
     return (
       <tbody aria-live="polite">
-        {usedOn.map(this.renderRow)}
+        <tr>
+          <td className={className} colSpan="3">{message}</td>
+        </tr>
       </tbody>
     );
   }
 
-  renderRow(data, index) {
-    const { id, type } = data;
-    const rowData = data.ancestors
-      ? [data].concat(data.ancestors).reverse()
-      : [data].reverse();
-    let cellLink = '#';
-    let isFirst = true;
-    const titleLinks = rowData.map((arr, i) => {
-      let title = arr.title;
-      const link = arr.link;
-      if (title && title.length >= 25) {
-        title = `${title.substring(0, 25).trim()}...`;
-      }
-      if (link) {
-        cellLink = link;
-      }
-      const key = `${index}-${id}-${i}`;
-      const cssClasses = ['used-on__title-item'];
-      if (isFirst) {
-        cssClasses.push('used-on__title-item--first');
-        isFirst = false;
-      }
-      return <li className={classnames(cssClasses)} key={key}>{title}</li>;
-    });
-    const key = `${index}-${id}`;
-    return (
-      <tr key={key} className="used-on__row">
-        <td className="used-on__col--index">
-          <a href={cellLink} className="used-on__cell-link">{index + 1}</a>
-        </td>
-        <td className="used-on__col--title">
-          <a href={cellLink} className="used-on__cell-link">
-            <ul className="used-on__title-items">{titleLinks}</ul>
-            <span className="used-on__type">{type}</span>
-          </a>
-        </td>
-      </tr>
-    );
-  }
+  return (
+    <tbody aria-live="polite">
+      {usedOn.map(renderRow)}
+    </tbody>
+  );
+};
 
-  render() {
-    return (
-      <table className="table used-on__table">
-        {this.renderHeader()}
-        {this.renderBody()}
-      </table>
-    );
-  }
-}
+const UsedOnTable = ({ usedOn, loading, error }) => (
+  <table className="table used-on__table">
+    {renderHeader()}
+    {renderBody(usedOn, loading, error)}
+  </table>
+);
 
 UsedOnTable.propTypes = {
   loading: PropTypes.bool,
--- client/src/components/UsedOnTable/tests/UsedOnTable-test.js
+++ client/src/components/UsedOnTable/tests/UsedOnTable-test.js
@@ -12,7 +12,7 @@ function makeProps(obj = {}) {
     tabContext: false,
     data: {
       readUsageEndpoint: {
-        url: 'http://www.bob.co.nz',
+        url: 'http://api.example.com',
         method: 'get',
       },
     },
@@ -32,9 +32,11 @@ test('UsedOnTable provideUsedOnData HOC should try to load data onMount', () => 
     expect(givenProps).toEqual(props);
     return null;
   });
-  const Provider = provideUsedOnData(mockComponent).Component;
+  const WrappedComponent = provideUsedOnData(mockComponent);
+  // This it to work with both class and functional components
+  const Provider = WrappedComponent.Component || WrappedComponent;
   render(<Provider {...props}/>);
-  expect(loadUsedOn).toBeCalledWith('abc', 'get', 'http://www.bob.co.nz');
+  expect(loadUsedOn).toBeCalledWith('abc', 'get', 'http://api.example.com');
   expect(mockComponent).toBeCalled();
 });
 
@@ -93,44 +95,33 @@ test('UsedOnTable should convert index to 1-based count', () => {
   expect(count.textContent).toBe('1');
 });
 
-test('UsedOnTable should add a link to table cells if the property is provided', () => {
-  const { container } = render(<UsedOnTable {...makeProps({
+test('UsedOnTable should add a link to table cells with correct priority (item > ancestor > hash)', () => {
+  const { container: container1 } = render(<UsedOnTable {...makeProps({
     usedOn: [
-      { id: 'abc', title: 'now I know', type: 'Page', ancestors: [], link: 'http://www.silverstripe.org/' },
+      { id: 'abc', title: 'Item Link', type: 'Page', ancestors: [], link: 'http://item.example.com/' },
     ]
   })}
   />);
-  const td = container.querySelectorAll('.used-on__cell-link');
-  expect(td[1].textContent).toContain('now I know');
-  expect(td[1].tagName).toBe('A');
-  expect(td[1].href).toBe('http://www.silverstripe.org/');
-});
+  const td1 = container1.querySelectorAll('.used-on__cell-link')[1];
+  expect(td1.href).toBe('http://item.example.com/');
 
-test('UsedOnTable should add a link to table cell if an link is blank and ancestor link is not', () => {
-  const { container } = render(<UsedOnTable {...makeProps({
+  const { container: container2 } = render(<UsedOnTable {...makeProps({
     usedOn: [
-      { id: 'abc', title: 'now I know', type: 'Page', ancestors: [{ title: 'Trees', link: 'http://www.silverstripe.org/trees' }], link: '' },
+      { id: 'abc', title: 'Ancestor Link', type: 'Page', ancestors: [{ title: 'Trees', link: 'http://trees.example.com/' }], link: '' },
     ]
   })}
   />);
-  const td = container.querySelectorAll('.used-on__cell-link');
-  expect(td[1].textContent).toContain('now I know');
-  expect(td[1].tagName).toBe('A');
-  expect(td[1].href).toBe('http://www.silverstripe.org/trees');
-});
+  const td2 = container2.querySelectorAll('.used-on__cell-link')[1];
+  expect(td2.href).toBe('http://trees.example.com/');
 
-test('UsedOnTable should add a hash link to table cells if no link is provided anywhere', () => {
-  const { container } = render(<UsedOnTable {...makeProps({
+  const { container: container3 } = render(<UsedOnTable {...makeProps({
     usedOn: [
-      { id: 'abc', title: 'now I know', type: 'Page', ancestors: [{ title: 'Flowers', link: '' }], link: '' },
+      { id: 'abc', title: 'No Link', type: 'Page', ancestors: [{ title: 'Flowers', link: '' }], link: '' },
     ]
   })}
   />);
-  const td = container.querySelectorAll('.used-on__cell-link');
-  expect(td[1].textContent).toContain('now I know');
-  expect(td[1].tagName).toBe('A');
-  // Will probably be "http://localhost/#"
-  expect(td[1].href.substr(-1)).toBe('#');
+  const td3 = container3.querySelectorAll('.used-on__cell-link')[1];
+  expect(td3.href.substr(-1)).toBe('#');
 });
 
 test('UsedOnTable should show the title provided', () => {
@@ -142,4 +133,81 @@ test('UsedOnTable should show the title provided', () => {
   />);
   const type = container.querySelectorAll('.used-on__type');
   expect(type[0].textContent).toContain('Boom!');
+});
+
+test('UsedOnTable should truncate long titles to 25 characters', () => {
+  const longTitle = 'This is a very long title that should be truncated';
+  const { container } = render(<UsedOnTable {...makeProps({
+    usedOn: [
+      { id: 'abc', title: longTitle, type: 'Page', ancestors: [] },
+    ]
+  })}
+  />);
+  const titleItem = container.querySelector('.used-on__title-item');
+  expect(titleItem.textContent).toBe('This is a very long title...');
+  expect(titleItem.textContent.length).toBe(28);
+});
+
+test('UsedOnTable should not truncate short titles', () => {
+  const { container } = render(<UsedOnTable {...makeProps({
+    usedOn: [
+      { id: 'abc', title: 'Short', type: 'Page', ancestors: [] },
+    ]
+  })}
+  />);
+  const titleItem = container.querySelector('.used-on__title-item');
+  expect(titleItem.textContent).toBe('Short');
+});
+
+test('UsedOnTable should render multiple rows', () => {
+  const { container } = render(<UsedOnTable {...makeProps({
+    usedOn: [
+      { id: 'abc', title: 'First', type: 'Page', ancestors: [] },
+      { id: 'def', title: 'Second', type: 'BlogPost', ancestors: [] },
+      { id: 'ghi', title: 'Third', type: 'File', ancestors: [] },
+    ]
+  })}
+  />);
+  const rows = container.querySelectorAll('.used-on__row');
+  expect(rows).toHaveLength(3);
+});
+
+test('UsedOnTable should render ancestors in correct order', () => {
+  const { container } = render(<UsedOnTable {...makeProps({
+    usedOn: [
+      {
+        id: 'abc',
+        title: 'Child',
+        type: 'Page',
+        ancestors: [
+          { title: 'Grandparent', link: 'http://grandparent.example.com' },
+          { title: 'Parent', link: 'http://parent.example.com' },
+        ],
+      },
+    ]
+  })}
+  />);
+  const titleItems = container.querySelectorAll('.used-on__title-item');
+  expect(titleItems[0].textContent).toBe('Parent');
+  expect(titleItems[1].textContent).toBe('Grandparent');
+  expect(titleItems[2].textContent).toBe('Child');
+});
+
+test('UsedOnTable should mark first title item with correct class', () => {
+  const { container } = render(<UsedOnTable {...makeProps({
+    usedOn: [
+      {
+        id: 'abc',
+        title: 'Child',
+        type: 'Page',
+        ancestors: [
+          { title: 'Parent', link: 'http://parent.example.com' },
+        ],
+      },
+    ]
+  })}
+  />);
+  const firstItem = container.querySelector('.used-on__title-item--first');
+  expect(firstItem).not.toBeNull();
+  expect(firstItem.textContent).toBe('Parent');
 });
